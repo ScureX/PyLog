22326b33ebeff44cd8f1bfb9f335b77689f8ced4
ScureX <47725553+ScureX@users.noreply.github.com>
2019-02-22 19:47:08 +0100

    Delete client.py

--- client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_addr = (('127.0.0.1', 3443))
-client_socket.connect(server_addr)
-client_socket.send(bytes("yeet", "utf8"))
